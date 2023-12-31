4eb094c47b465dfe00aa72823d6795f357236641
EasyJake <easyj4ke@gmail.com>
2023-12-30 21:18:07 -0800

    webpack compiled successfully

--- rr-3-react-a-mole.sh
+++ rr-3-react-a-mole.sh
@@ -1,119 +1,153 @@
+# # # # # #!/bin/bash
+
+# # # # # # Define the project directory
+# # # # # PROJECT_DIR="react_a_mole"
+
+# # # # # # Create a new React application
+# # # # # npx create-react-app $PROJECT_DIR
+
+# # # # # # Navigate into the project directory
+# # # # # cd $PROJECT_DIR
+
+# # # # # # Remove the boilerplate code (Assuming standard CRA structure)
+# # # # # rm -rf src/*
+
+# # # # # # Clone the image assets repository
+# # # # # git clone https://github.com/HackerUSA-CE/RR-React-A-Mole-Images src/assets
+
+# # # # # # Notify the user
+# # # # # echo "Setup complete. The project directory is $PROJECT_DIR"
+# # # # # echo "Image assets have been downloaded to $PROJECT_DIR/src/assets"
+
+# # # # #!/bin/bash
+
+# # # # # Navigate to the project directory
+# # # # cd react_a_mole
+
+# # # # # Remove boilerplate code, but leave index.js and App.js intact
+# # # # find src -type f ! -name 'index.js' ! -name 'App.js' -delete
+
+# # # # # Clean up index.js to only include necessary imports and rendering of the App component
+# # # # cat > src/index.js <<EOL
+# # # # import React from 'react';
+# # # # import ReactDOM from 'react-dom';
+# # # # import './index.css';
+# # # # import App from './App';
+
+# # # # ReactDOM.render(
+# # # #   <React.StrictMode>
+# # # #     <App />
+# # # #   </React.StrictMode>,
+# # # #   document.getElementById('root')
+# # # # );
+# # # # EOL
+
+# # # # # Modify App.js to be a functional component with a basic structure
+# # # # cat > src/App.js <<EOL
+# # # # import React from 'react';
+
+# # # # function App() {
+# # # #   return (
+# # # #     <div className="App">
+# # # #       {/* Your game components will go here */}
+# # # #     </div>
+# # # #   );
+# # # # }
+
+# # # # export default App;
+# # # # EOL
+
+# # # # # Create the necessary component files
+# # # # mkdir -p src/components
+# # # # touch src/components/MoleContainer.js
+# # # # touch src/components/Mole.js
+# # # # touch src/components/EmptySlot.js
+
+# # # # # Clone image assets into the src/assets directory if not already present
+# # # # if [ ! -d "src/assets" ]; then
+# # # #   git clone https://github.com/HackerUSA-CE/RR-React-A-Mole-Images src/assets
+# # # # fi
+
+# # # # # Notify the user
+# # # # echo "Setup and component file creation complete."
+# # # # echo "You can now start developing your components within the src/components directory."
+# # # # echo "Don't forget to import and use them in App.js!"
+
 # # # #!/bin/bash
 
-# # # # Define the project directory
+# # # # Define the project directory name
 # # # PROJECT_DIR="react_a_mole"
 
-# # # # Create a new React application
-# # # npx create-react-app $PROJECT_DIR
+# # # # Check if the project directory exists
+# # # if [ -d "$PROJECT_DIR" ]; then
+# # #   # Move into the project directory
+# # #   cd "$PROJECT_DIR"
 
-# # # # Navigate into the project directory
-# # # cd $PROJECT_DIR
+# # #   # Move everything up one directory
+# # #   mv -v * .[^.]* ..
 
-# # # # Remove the boilerplate code (Assuming standard CRA structure)
-# # # rm -rf src/*
+# # #   # Move back to parent directory
+# # #   cd ..
 
-# # # # Clone the image assets repository
-# # # git clone https://github.com/HackerUSA-CE/RR-React-A-Mole-Images src/assets
+# # #   # Remove the now empty project directory
+# # #   rmdir "$PROJECT_DIR"
 
-# # # # Notify the user
-# # # echo "Setup complete. The project directory is $PROJECT_DIR"
-# # # echo "Image assets have been downloaded to $PROJECT_DIR/src/assets"
+# # #   echo "All files have been moved up one directory."
+# # # else
+# # #   echo "The directory $PROJECT_DIR does not exist."
+# # # fi
+
 
 # # #!/bin/bash
 
-# # # Navigate to the project directory
-# # cd react_a_mole
+# # # Navigate to the src directory of your React project
+# # cd path/to/your/react_a_mole/src
 
-# # # Remove boilerplate code, but leave index.js and App.js intact
-# # find src -type f ! -name 'index.js' ! -name 'App.js' -delete
+# # # Check if the assets directory exists and is empty
+# # if [ -d "assets" ] && [ ! "$(ls -A assets)" ]; then
+# #   echo "The assets directory is empty. Attempting to clone assets..."
 
-# # # Clean up index.js to only include necessary imports and rendering of the App component
-# # cat > src/index.js <<EOL
-# # import React from 'react';
-# # import ReactDOM from 'react-dom';
-# # import './index.css';
-# # import App from './App';
+# #   # Remove the existing empty assets directory
+# #   rm -rf assets
 
-# # ReactDOM.render(
-# #   <React.StrictMode>
-# #     <App />
-# #   </React.StrictMode>,
-# #   document.getElementById('root')
-# # );
-# # EOL
+# #   # Clone the assets from the GitHub repository
+# #   git clone https://github.com/HackerUSA-CE/RR-React-A-Mole-Images assets
 
-# # # Modify App.js to be a functional component with a basic structure
-# # cat > src/App.js <<EOL
-# # import React from 'react';
-
-# # function App() {
-# #   return (
-# #     <div className="App">
-# #       {/* Your game components will go here */}
-# #     </div>
-# #   );
-# # }
-
-# # export default App;
-# # EOL
-
-# # # Create the necessary component files
-# # mkdir -p src/components
-# # touch src/components/MoleContainer.js
-# # touch src/components/Mole.js
-# # touch src/components/EmptySlot.js
-
-# # # Clone image assets into the src/assets directory if not already present
-# # if [ ! -d "src/assets" ]; then
-# #   git clone https://github.com/HackerUSA-CE/RR-React-A-Mole-Images src/assets
+# #   echo "Assets have been cloned."
+# # else
+# #   echo "The assets directory is not empty or does not exist. No action taken."
 # # fi
-
-# # # Notify the user
-# # echo "Setup and component file creation complete."
-# # echo "You can now start developing your components within the src/components directory."
-# # echo "Don't forget to import and use them in App.js!"
 
 # #!/bin/bash
 
-# # Define the project directory name
-# PROJECT_DIR="react_a_mole"
+# # Navigate to the src directory
+# cd src
 
-# # Check if the project directory exists
-# if [ -d "$PROJECT_DIR" ]; then
-#   # Move into the project directory
-#   cd "$PROJECT_DIR"
+# # Check if the index.css file already exists
+# if [ ! -f "index.css" ]; then
+#     # If the file does not exist, create it with basic content
+#     cat <<EOL > index.css
+# /* Add your global styles here */
+# body {
+#   margin: 0;
+#   padding: 0;
+#   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+# }
 
-#   # Move everything up one directory
-#   mv -v * .[^.]* ..
+# /* Styles for the main application container */
+# .App {
+#   text-align: center;
+# }
 
-#   # Move back to parent directory
-#   cd ..
+# /* Add more styles as needed */
+# EOL
 
-#   # Remove the now empty project directory
-#   rmdir "$PROJECT_DIR"
-
-#   echo "All files have been moved up one directory."
+#     echo "index.css file created successfully."
 # else
-#   echo "The directory $PROJECT_DIR does not exist."
+#     echo "index.css file already exists."
 # fi
 
+# # Navigate back to the project root
+# cd ..
 
-#!/bin/bash
 
-# Navigate to the src directory of your React project
-cd path/to/your/react_a_mole/src
-
-# Check if the assets directory exists and is empty
-if [ -d "assets" ] && [ ! "$(ls -A assets)" ]; then
-  echo "The assets directory is empty. Attempting to clone assets..."
-
-  # Remove the existing empty assets directory
-  rm -rf assets
-
-  # Clone the assets from the GitHub repository
-  git clone https://github.com/HackerUSA-CE/RR-React-A-Mole-Images assets
-
-  echo "Assets have been cloned."
-else
-  echo "The assets directory is not empty or does not exist. No action taken."
-fi
